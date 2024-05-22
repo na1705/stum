09b03dcae71b928e3728931439763b1e701de3d1
Chan11203 <trangdt11203@gmail.com>
2024-05-23 00:29:50 +0700

    create_list_ui

---
+++ Controllers/studentsController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using StuMSystem.Models;
+
+namespace StuMSystem.Controllers
+{
+    public class studentsController : Controller
+    {
+        private StuMEntities db = new StuMEntities();
+
+        // GET: students
+        public ActionResult Index()
+        {
+            var students = db.students.Include(s => s.user);
+            return View(students.ToList());
+        }
+
+        // GET: students/Search/5
+        public ActionResult Search(int id)
+        {
+            student student = db.students.Find(id);
+            return View(student);
+        }
+
+        // GET: students/Create
+        public ActionResult Create()
+        {
+            ViewBag.userId = new SelectList(db.users, "id", "userName");
+            return View();
+        }
+
+        // POST: students/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "studentId,userId,studentName,gender,email,address,tel,major,dateofbirth")] student student)
+        {
+            if (ModelState.IsValid)
+            {
+                db.students.Add(student);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.userId = new SelectList(db.users, "id", "userName", student.userId);
+            return View(student);
+        }
+
+        // GET: students/Edit/5
+        public ActionResult Edit(int id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            student student = db.students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.userId = new SelectList(db.users, "id", "userName", student.userId);
+            return View(student);
+        }
+
+        // POST: students/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "studentId,userId,studentName,gender,email,address,tel,major,dateofbirth")] student student)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(student).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.userId = new SelectList(db.users, "id", "userName", student.userId);
+            return View(student);
+        }
+
+        // GET: students/Delete/5
+        public ActionResult Delete(int id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            student student = db.students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // POST: students/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            student student = db.students.Find(id);
+            db.students.Remove(student);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
